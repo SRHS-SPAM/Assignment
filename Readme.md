5659daa23103a8a7aa00c8b6325fdb84ef8fb5e1
igiza1213 <igiza1213@gmail.com>
2023-10-30 21:03:27 +0900

    0.1.1-alpha

--- Readme.md
+++ Readme.md
@@ -1,1 +1,6 @@
-# 넥스트
+# SPAM의 Assignment를 제출하는 레포입니다.
+
+- front_assignment에서 html, css에 관한 과제를 제출합니다.
+- next_assignment엣 next.js관한 과제를 제출합니다.
+
+각 과제 참고사항은 해당 README 참고
--- next_assignment/1/submit/app/page.jsx
+++ next_assignment/1/submit/app/page.jsx
@@ -1,6 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div></div>;
+  const nameList = [
+    { name: "지민철", url: "/" },
+    { name: "김도원", url: "/" },
+  ];
+  return (
+    <main>
+      <div className={styles.up_box}>
+        <Link href={"/assignment"}>
+          <div className={styles.announcement}>과제 보러가기!</div>
+        </Link>
+      </div>
+      <div className={styles.down_box}>
+        {nameList.map((arg, i) => (
+          <Link href={arg.url} key={i}>
+            <div className={styles.name_box}>{arg.name}</div>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 }

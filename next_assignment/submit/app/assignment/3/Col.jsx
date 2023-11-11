f316ecdcb601ee84fb1e424be10760e78c588f7b
igiza1213 <igiza1213@gmail.com>
2023-11-12 00:56:57 +0900

    c

--- next_assignment/submit/app/assignment/3/Col.jsx
+++ next_assignment/submit/app/assignment/3/Col.jsx
@@ -1,7 +1,9 @@
 "use client";
+import { useState } from "react";
 import style from "./page.module.css";
 
 const Col = (props) => {
+  const [clicked, setClicked] = useState(false);
   const col = props.col;
   const colIndex = props.colIndex;
   const rowIndex = props.rowIndex;
@@ -10,19 +12,21 @@ const Col = (props) => {
       <div
         className={`${style.col} ${
           (rowIndex + colIndex) % 2 == 0 ? style.even : style.odd
-        }`}
+        } ${clicked == true ? style.clicked : ""}`}
         onClick={() => {
           fetch("/api/assignment/chess", {
             method: "POST",
             body: JSON.stringify({
-              i: rowIndex,
-              j: colIndex,
+              rowIndex: rowIndex,
+              colIndex: colIndex,
             }),
           })
             .then((r) => r.json())
             .then((r) => {
-              JSON.parse(r);
-              console.log(r);
+              const result = JSON.parse(r);
+              if (colIndex == result.colIndex && rowIndex == result.rowIndex) {
+                setClicked(true);
+              }
             });
         }}
       >
